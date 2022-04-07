cfb1202c1214688889e3a4fcb382e27b12d0b196
Wilhelm <wilvo970@student.liu.se>
2022-04-07 19:12:52 +0200

    Changed container for arguments list

--- text_editor.cc
+++ text_editor.cc
@@ -6,53 +6,31 @@
 #include <iterator>
 #include <queue>
 
-template <typename T>
-class Queue_Adapter             // Snott från stackoverflow, är "adapter" en grej och isf vad innebär att en klass är adapter?
-{
-public:
-    using value_type = T;
+using str_pair = std::pair<std::string, std::string>;
 
-    Queue_Adapter(std::queue<T> & q)
-        : q{q}
-    {}
-    void push_back(T const& element)
-    {
-        q.push(element);
-    }
-private:
-    std::queue<T>& q;
-};
+void handle_arguments(str_pair & args_p, std::vector<std::string> & text);
 
-void insert_args_to_queue(Queue_Adapter<std::pair<std::string, std::string>> & Q, std::vector<std::string> & arguments, std::vector<std::string> & text);
-void execute_args(std::queue<std::pair<std::string, std::string>> & args_queue, std::vector<std::string> & text);
+void do_remove(std::vector<std::string> & text);
+void do_substitute(std::vector<std::string> & text);
 
+void do_print(std::vector<std::string> const& text);
+void do_table(std::vector<std::string> const& text);
+void do_freqtable(std::vector<std::string> const& text);
 
 int main(int argc, char* argv[])
 {
     if (argc < 2)
     {
-        std::cout << "Please write: ./a.out <filename> <args>" << std::endl;
+        std::cout << "Please write: ./a.out <filename> <args>[=]<paremeter>+<value>" << std::endl;
         return 1;
     }
     std::fstream in_file{argv[1], std::ios::in};
     
-    std::vector<std::string> arguments{argv + 2, argv + argc}; // Varför inte begin(argv), end(argv)?
+    std::vector<std::string> arguments{argv + 2, argv + argc};
     std::vector<std::string> text{std::istream_iterator<std::string>(in_file), std::istream_iterator<std::string>()};
-    
-    std::queue<std::pair<std::string, std::string>> args_queue{}; 
-    Queue_Adapter<std::pair<std::string, std::string>> Q{args_queue};       // Used to make push_back available for std::back_inserter
-
-    // Effectively fills args_queue with argument-parameter pairs
-    insert_args_to_queue(Q, arguments, text);                               
-    
-    execute_args(args_queue, text);
-
-    return 0;
-}
-
-void insert_args_to_queue(Queue_Adapter<std::pair<std::string, std::string>> & Q, std::vector<std::string> & arguments, std::vector<std::string> & text)
-{
-    std::transform(begin(arguments), end(arguments), std::back_inserter(Q), [](std::string & line)
+                               
+    std::vector<str_pair> args_pair_list(arguments.size());
+    std::transform(begin(arguments), end(arguments), begin(args_pair_list), [](std::string & line)
                                                                 {
                                                                     auto eq_it{find(begin(line), end(line), '=')};
                                                                     std::string flag{begin(line), eq_it};
@@ -61,23 +39,53 @@ void insert_args_to_queue(Queue_Adapter<std::pair<std::string, std::string>> & Q
                                                                     if (eq_it != end(line))
                                                                         copy(eq_it + 1, end(line), std::back_inserter(parameter));
                                                                       
-                                                                    return std::pair<std::string, std::string>{flag, parameter};
+                                                                    return str_pair{flag, parameter};
                                                                 });
+
+    std::for_each(begin(args_pair_list), end(args_pair_list), [&text](str_pair& args_p)
+                                                    {
+                                                       handle_arguments(args_p, text);
+                                                    });
+
+    return 0;
 }
 
-void execute_args(std::queue<std::pair<std::string, std::string>> & args_queue, std::vector<std::string> & text)
+void handle_arguments(str_pair & args_p, std::vector<std::string> & text)
 {
-
-    while (!args_queue.empty())
+    if (args_p.first == "--remove")
     {
-        std::cout << args_queue.front().first << " " << args_queue.front().second << std::endl;
-        args_queue.pop();
+        // TODO
+    }
+    else if (args_p.first == "--print")
+    {
+        do_print(text);
+    }
+    else if (args_p.first == "--frequency")
+    {
+        do_freqtable(text);
+    }
+    else if (args_p.first == "--table")
+    {
+        do_table(text);
+    }
+    else if (args_p.first == "--substitute")
+    {
+        // TODO
     }
 }
 
+void do_print(std::vector<std::string> const& text)
+{
+    std::copy(begin(text), end(text), std::ostream_iterator<std::string>(std::cout, " "));
+    std::cout << std::endl;
+}
 
-    // std::copy(begin(text), end(text), std::ostream_iterator<std::string>(std::cout, " "));
-    // for(string const& e : arguments)
-    // {
-        // cout << e << endl;
-    // }
+void do_table(std::vector<std::string> const& text)
+{
+
+}
+
+void do_freqtable(std::vector<std::string> const& text)
+{
+
+}
